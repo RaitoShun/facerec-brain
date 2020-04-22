ec4ab37c021d416cd6a32d321d8be30322ed0d07
RaitoShun <abrahamthiao@gmail.com>
2020-04-22 17:29:14 +0200

    performance++; engineering--;

--- src/components/FaceRec/FaceRec.js
+++ src/components/FaceRec/FaceRec.js
@@ -6,7 +6,7 @@ import './FaceRec.css';
 const FaceRec = ({ imageURL,boks }) => {
 	return(
 		<div className='center ma'>
-			<div className='absolute mt2'>
+			<div className='absolute mt0 ma4'>
 				<img id='inputImg' src={imageURL} alt="" width='500px' height='auto'/>
 				<div className="bounding-box" style={{top: boks.topRow,right: boks.rightCol,left: boks.leftCol,bottom: boks.bottomRow}}></div>
 			</div>
